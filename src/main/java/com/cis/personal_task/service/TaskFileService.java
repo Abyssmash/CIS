8d6be09f379cd499960cc8083cef133dfd518613
Abyssmash <lime920506@gmail.com>
2024-12-19 02:46:59 +0900

    Mod: 개인 업무 파일 서비스 수정 241215

--- src/main/java/com/cis/personal_task/service/TaskFileService.java
+++ src/main/java/com/cis/personal_task/service/TaskFileService.java
@@ -1,6 +1,9 @@
 package com.cis.personal_task.service;
 
 import com.cis.personal_task.dto.TaskFileDTO;
+import com.cis.personal_task.exception.FileUploadException;
+import jakarta.annotation.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -9,15 +12,6 @@ import java.util.List;
 
 public interface TaskFileService {
 
-    // 파일 저장
-    void saveTaskFile(int task_num, List<MultipartFile> files) throws Exception;
 
-    // 파일 조회 (업무 ID와 파일명으로 특정 파일 조회)
-    TaskFileDTO findTaskFile(int task_num, String file_name);
-
-    // 파일 다운로드
-    File downloadFile(int taskNum, String fileName) throws IOException;
-
-    // 업무 파일 목록 조회 (업무 ID로 해당 업무에 속한 모든 파일 목록 조회)
-    List<TaskFileDTO> findTaskFilesByTaskId(int taskNum);
+    String saveFile(MultipartFile file) throws FileUploadException;
 }
